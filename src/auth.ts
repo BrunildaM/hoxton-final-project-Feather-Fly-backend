aa85c4e3a066b815d95a18b3a978d552004ad04f
Brunilda Muco <brunildamuco6@gmail.com>
2022-10-15 19:55:44 +0200

    auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,20 +1,35 @@
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-import dotenv from "dotenv"
-dotenv.config()
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+dotenv.config();
+const prisma = new PrismaClient();
+const SECRET = process.env.SECRET!;
 
-export function hash (password: string) {
-   const hashedPass = bcrypt.hashSync(password, 12)
-   return hashedPass 
+export function hash(password: string) {
+  const hashedPass = bcrypt.hashSync(password, 12);
+  return hashedPass;
 }
 
-export function verify (password: string, hash: string) {
-  return  bcrypt.compareSync(password, hash)
+export function verify(password: string, hash: string) {
+  return bcrypt.compareSync(password, hash);
 }
 
-export function generateToken (id: number) {
- return  jwt.sign({id}, process.env.SECRET!)
-
+export function generateToken(id: number) {
+  const token = jwt.sign({ id }, SECRET, { expiresIn: "1h" });
+  return token;
 }
 
-export function getCurrentUser () {}
+export async function getCurrentUser(token: string) {
+  try {
+    const data = jwt.verify(token, SECRET);
+    const user = await prisma.user.findUnique({
+      //@ts-ignore
+      where: { id: data.id },
+      include: { bookings: true },
+    });
+    return user;
+  } catch (error) {
+    return null;
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
-import { generateToken, hash } from "./auth";
+import { generateToken, getCurrentUser, hash } from "./auth";
+import bcrypt from 'bcryptjs'
 import { verify } from "jsonwebtoken";
 
 const app = express();
 app.use(cors());
+app.options("*", cors())
 app.use(express.json());
 const prisma = new PrismaClient();
 
@@ -41,57 +43,133 @@ app.get("/flights", async (req, res) => {
 
 app.post("/flights", async (req, res) => {});
 
-//Log-in a user that already exists with it's credentials
-//Check if they email exists on our db
-//Check if the e-mail and password match
-app.post("sign-in", async (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
 
-  const user = await prisma.user.findUnique({ where: { email } });
-  if (user && verify(password, user.password)) {
-    const token = generateToken(user.id);
-    res.send({ user, token });
+
+//Log-in a user that already exists with it's credentials
+app.post("/sign-in", async (req, res) => {
+  try {
+    const {email, password} = req.body;
+    
+
+    //check for any possible error
+    const errors: string[] = [];
+
+    if (typeof email !== "string")
+      errors.push("Email not provided or not a string!");
+
+    if (typeof password !== "string")
+      errors.push("Password not provided or not a string!");
+
+    if (errors.length > 0) {
+      return res.status(400).send({ errors });
+    }
+
+    //Check if they email exists on our db
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    //Check if the e-mail and password match
+    if (user && verify(password, user.password)) {
+      const token = generateToken(user.id);
+      res.send({ user, token });
+    } else {
+      res.status(400).send({ errors: ["Wrong credentials!"] });
+    }
+  } catch (error) {
+    //@ts-ignore
+    res.status(400).send({ errors: [error.message] });
   }
 });
 
 //Create a new account for users
-//Don't allow a user under 18 to create an account- needs to be done
-app.post("sign-up", async (req, res) => {
-  const { email, password, gender, age, firstName, lastName } = req.body;
+app.post("/sign-up", async (req, res) => {
+  const { email, password, gender, age, firstName, lastName, role } = req.body;
   try {
-
     //Check if the email is already in our db
     const existingUser = await prisma.user.findUnique({
       where: { email },
     });
 
+    //Checking all possible errors
+    const errors: string[] = [];
+    if (typeof email !== "string")
+      errors.push("Email not provided or not a string");
+
+    if (typeof password !== "string")
+      errors.push("Password not provided or not a string");
+
+    if (typeof gender !== "string")
+      errors.push("Gender not provided or not a string");
+
+    if (typeof age !== "number")
+      errors.push("Age not provided or not a number");
+
+    if (typeof firstName !== "string")
+      errors.push("First name not provided or not a string");
+
+    if (typeof lastName !== "string")
+      errors.push("Last name not provided or not a string");
+
+    if (typeof role !== "string")
+      errors.push("Role not provided or not a string!");
+
+    //Don't allow a user under 18 to create an account
+    if (age < 18)
+      errors.push("You can't create an account if you are under 18!");
+
+    if (errors.length > 0) {
+      return res.status(400).send({ errors });
+    }
+
     //Make sure they don't create two accounts with the same email
     if (existingUser) {
-      return res.status(400).send({ error: "User already exists!" });
-    } 
-      const userData = {
-        gender,
-        age,
-        firstName,
-        lastName,
-        email,
-        password: hash(password),
-      };
+      return res.status(400).send({ errors: ["User already exists!"] });
+    }
 
-      const newUser = await prisma.user.create({
-        data: userData,
-      });
+    const userData = {
+      gender,
+      age,
+      firstName,
+      lastName,
+      email,
+      password: hash(password),
+      role
+    };
 
-      const token = generateToken(newUser.id);
-      res.send({ newUser, token });
+    const newUser = await prisma.user.create({
+      data: userData,
+    });
+
+    const token = generateToken(newUser.id);
+    res.send({ newUser, token });
   } catch (error) {
     //@ts-ignore
-    res.status(400).send({ error: error.message });
+    res.status(400).send({ errors: [error.message] });
   }
 });
 
-app.get("validate");
+app.get("/validate", async (req, res) => {
+  try {
+    //get the token
+    const token = req.headers.authorization;
+    if (token) {
+      const user = await getCurrentUser(token);
+      //check if there is a user with this token
+      if (user) {
+        const newToken = generateToken(user.id);
+        //send the user with a new token
+        res.send({ user, token: newToken });
+      } else {
+        //check all possible errors
+        res.status(400).send({ errors: ["Invalid token!"] });
+      }
+    } else {
+      res.status(400).send({ errors: ["Token not provided "] });
+    }
+  } catch (error) {
+    //@ts-ignore
+    res.status(400).send({ errors: [error.message] });
+  }
+});
 
 app.listen(port, () => {
   console.log(`App is running: http://localhost:${port}`);
